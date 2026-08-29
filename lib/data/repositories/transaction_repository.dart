f054051d2a8d6c91a980b9d2199bc1b2d01dc203
Ahsan <ahsanlma5e@gmail.com>
2026-08-29 21:15:44 +0300

    Add transaction repository

---
+++ lib/data/repositories/transaction_repository.dart
@@ -0,0 +1,18 @@
+import 'package:khatabook_lite/domain/entities/transaction.dart';
+
+abstract class TransactionRepository {
+  Future<List<Transaction>> getTransactionsByCustomer(String customerId);
+  Future<List<Transaction>> getAllTransactions();
+  Future<Transaction> addTransaction({
+    required String customerId,
+    required TransactionType type,
+    required double amount,
+    String? note,
+    String? voiceNotePath,
+    String? photoPath,
+  });
+  Future<double> getCustomerBalance(String customerId);
+  Future<double> getTotalOwedToVendor();
+  Future<double> getTotalVendorOwes();
+  Future<void> deleteTransaction(String id);
+}
