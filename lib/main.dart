a0e40e18c4b4aa36746b42b8afb73bc66617dc75
Ahsan <ahsanlma5e@gmail.com>
2026-08-30 08:40:21 +0500

    Update main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'core/theme/app_theme.dart';
+import 'app.dart';
 import 'data/models/customer_model.dart';
 import 'data/models/transaction_model.dart';
 import 'data/repositories/customer_repository_impl.dart';
@@ -13,9 +12,6 @@ import 'domain/usecases/get_customer_balance.dart';
 import 'domain/usecases/get_customers.dart';
 import 'domain/usecases/get_dashboard_data.dart';
 import 'domain/usecases/get_transactions.dart';
-import 'presentation/bloc/customer/customer_bloc.dart';
-import 'presentation/bloc/transaction/transaction_bloc.dart';
-import 'presentation/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -60,58 +56,4 @@ void main() async {
       getCustomerBalance: getCustomerBalance,
     ),
   );
-}
-
-class KhataBookLiteApp extends StatelessWidget {
-  final CustomerRepositoryImpl customerRepository;
-  final TransactionRepositoryImpl transactionRepository;
-  final GetCustomers getCustomers;
-  final AddCustomer addCustomer;
-  final DeleteCustomer deleteCustomer;
-  final GetTransactions getTransactions;
-  final AddTransaction addTransaction;
-  final GetDashboardData getDashboardData;
-  final GetCustomerBalance getCustomerBalance;
-
-  const KhataBookLiteApp({
-    super.key,
-    required this.customerRepository,
-    required this.transactionRepository,
-    required this.getCustomers,
-    required this.addCustomer,
-    required this.deleteCustomer,
-    required this.getTransactions,
-    required this.addTransaction,
-    required this.getDashboardData,
-    required this.getCustomerBalance,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CustomerBloc>(
-          create: (context) => CustomerBloc(
-            getCustomers: getCustomers,
-            addCustomer: addCustomer,
-            deleteCustomer: deleteCustomer,
-          ),
-        ),
-        BlocProvider<TransactionBloc>(
-          create: (context) => TransactionBloc(
-            getTransactions: getTransactions,
-            addTransaction: addTransaction,
-            getDashboardData: getDashboardData,
-            getCustomerBalance: getCustomerBalance,
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'KhataBook Lite',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.light,
-        home: const HomeScreen(),
-      ),
-    );
-  }
 }
