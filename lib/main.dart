7da37f625ba22cc5d9183b6d13387290e6cf3298
Ahsan <ahsanlma5e@gmail.com>
2026-09-02 13:38:46 +0500

    update main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:khatabook_lite/app.dart';
+import 'package:khatabook_lite/data/models/customer_model.dart';
+import 'package:khatabook_lite/data/models/transaction_model.dart';
+import 'package:khatabook_lite/data/repositories/customer_repository_impl.dart';
+import 'package:khatabook_lite/data/repositories/transaction_repository_impl.dart';
+import 'package:khatabook_lite/domain/usecases/add_customer.dart';
+import 'package:khatabook_lite/domain/usecases/add_transaction.dart';
+import 'package:khatabook_lite/domain/usecases/delete_customer.dart';
 import 'package:khatabook_lite/domain/usecases/delete_transaction.dart';
-import 'app.dart';
-import 'data/models/customer_model.dart';
-import 'data/models/transaction_model.dart';
-import 'data/repositories/customer_repository_impl.dart';
-import 'data/repositories/transaction_repository_impl.dart';
-import 'domain/usecases/add_customer.dart';
-import 'domain/usecases/add_transaction.dart';
-import 'domain/usecases/delete_customer.dart';
-import 'domain/usecases/get_customer_balance.dart';
-import 'domain/usecases/get_customers.dart';
-import 'domain/usecases/get_dashboard_data.dart';
-import 'domain/usecases/get_transactions.dart';
+import 'package:khatabook_lite/domain/usecases/get_customer_balance.dart';
+import 'package:khatabook_lite/domain/usecases/get_customers.dart';
+import 'package:khatabook_lite/domain/usecases/get_dashboard_data.dart';
+import 'package:khatabook_lite/domain/usecases/get_transactions.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
 
   // Initialize Hive
   await Hive.initFlutter();
@@ -41,23 +43,28 @@ void main() async {
   final deleteCustomer = DeleteCustomer(customerRepository);
   final getTransactions = GetTransactions(transactionRepository);
   final addTransaction = AddTransaction(transactionRepository);
+  final deleteTransaction = DeleteTransaction(transactionRepository);
   final getDashboardData = GetDashboardData(transactionRepository);
   final getCustomerBalance = GetCustomerBalance(transactionRepository);
-  // Inside main() after other use cases:
-  final deleteTransaction = DeleteTransaction(transactionRepository);
 
   runApp(
-    KhataBookLiteApp(
-      customerRepository: customerRepository,
-      transactionRepository: transactionRepository,
-      getCustomers: getCustomers,
-      addCustomer: addCustomer,
-      deleteCustomer: deleteCustomer,
-      getTransactions: getTransactions,
-      addTransaction: addTransaction,
-      getDashboardData: getDashboardData,
-      getCustomerBalance: getCustomerBalance,
-      deleteTransaction: deleteTransaction,
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ur')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      startLocale: const Locale('en'),
+      child: KhataBookLiteApp(
+        customerRepository: customerRepository,
+        transactionRepository: transactionRepository,
+        getCustomers: getCustomers,
+        addCustomer: addCustomer,
+        deleteCustomer: deleteCustomer,
+        getTransactions: getTransactions,
+        addTransaction: addTransaction,
+        deleteTransaction: deleteTransaction,
+        getDashboardData: getDashboardData,
+        getCustomerBalance: getCustomerBalance,
+      ),
     ),
   );
 }
