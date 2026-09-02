76fe563c4218d2398bc5bb7984251df5cd64e7f5
Ahsan <ahsanlma5e@gmail.com>
2026-09-02 13:39:36 +0500

    Update app.dart file

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,19 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:khatabook_lite/core/theme/app_theme.dart';
+import 'package:khatabook_lite/data/repositories/customer_repository_impl.dart';
+import 'package:khatabook_lite/data/repositories/transaction_repository_impl.dart';
+import 'package:khatabook_lite/domain/usecases/add_customer.dart';
+import 'package:khatabook_lite/domain/usecases/add_transaction.dart';
+import 'package:khatabook_lite/domain/usecases/delete_customer.dart';
 import 'package:khatabook_lite/domain/usecases/delete_transaction.dart';
-import 'core/theme/app_theme.dart';
-import 'data/repositories/customer_repository_impl.dart';
-import 'data/repositories/transaction_repository_impl.dart';
-import 'domain/usecases/add_customer.dart';
-import 'domain/usecases/add_transaction.dart';
-import 'domain/usecases/delete_customer.dart';
-import 'domain/usecases/get_customer_balance.dart';
-import 'domain/usecases/get_customers.dart';
-import 'domain/usecases/get_dashboard_data.dart';
-import 'domain/usecases/get_transactions.dart';
-import 'presentation/bloc/customer/customer_bloc.dart';
-import 'presentation/bloc/transaction/transaction_bloc.dart';
+import 'package:khatabook_lite/domain/usecases/get_customer_balance.dart';
+import 'package:khatabook_lite/domain/usecases/get_customers.dart';
+import 'package:khatabook_lite/domain/usecases/get_dashboard_data.dart';
+import 'package:khatabook_lite/domain/usecases/get_transactions.dart';
+import 'package:khatabook_lite/presentation/bloc/customer/customer_bloc.dart';
+import 'package:khatabook_lite/presentation/bloc/transaction/transaction_bloc.dart';
 import 'package:khatabook_lite/presentation/screens/home_screen.dart';
 
 class KhataBookLiteApp extends StatelessWidget {
@@ -23,10 +24,9 @@ class KhataBookLiteApp extends StatelessWidget {
   final DeleteCustomer deleteCustomer;
   final GetTransactions getTransactions;
   final AddTransaction addTransaction;
+  final DeleteTransaction deleteTransaction;
   final GetDashboardData getDashboardData;
   final GetCustomerBalance getCustomerBalance;
-  // Add to class properties:
-  final DeleteTransaction deleteTransaction;
 
   const KhataBookLiteApp({
     super.key,
@@ -37,9 +37,9 @@ class KhataBookLiteApp extends StatelessWidget {
     required this.deleteCustomer,
     required this.getTransactions,
     required this.addTransaction,
+    required this.deleteTransaction,
     required this.getDashboardData,
     required this.getCustomerBalance,
-    required this.deleteTransaction,
   });
 
   @override
@@ -67,6 +67,9 @@ class KhataBookLiteApp extends StatelessWidget {
         title: 'KhataBook Lite',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.light,
+        locale: context.locale,
+        supportedLocales: context.supportedLocales,
+        localizationsDelegates: context.localizationDelegates,
         home: const HomeScreen(),
       ),
     );
