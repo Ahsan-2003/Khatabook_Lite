c678764a10954b542f5eea9fbdcf9ef64ddd96fb
Ahsan <ahsanlma5e@gmail.com>
2026-09-06 21:40:47 +0300

    degrade flutter_contacts dependency package version

--- lib/presentation/screens/contact_import_screen.dart
+++ lib/presentation/screens/contact_import_screen.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:khatabook_lite/core/theme/app_colors.dart';
 import 'package:khatabook_lite/core/theme/app_text_styles.dart';
 import 'package:khatabook_lite/presentation/bloc/customer/customer_bloc.dart';
@@ -51,6 +50,18 @@ class _ContactImportScreenState extends State<ContactImportScreen> {
 
   Future<void> _loadContacts() async {
     try {
+      // Request permission using flutter_contacts
+      final granted = await FlutterContacts.requestPermission();
+
+      if (!granted) {
+        setState(() {
+          _hasPermission = false;
+          _isLoading = false;
+        });
+        return;
+      }
+
+      // Get all contacts
       final contacts = await FlutterContacts.getContacts(
         withProperties: true,
         withPhoto: false,
@@ -72,6 +83,7 @@ class _ContactImportScreenState extends State<ContactImportScreen> {
         _contacts = validContacts;
         _filteredContacts = validContacts;
         _isLoading = false;
+        _hasPermission = true;
       });
     } catch (e) {
       setState(() {
