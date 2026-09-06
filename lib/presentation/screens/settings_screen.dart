649be4c0d2f6ca328f72936780bd215dd3b36c5a
Ahsan <ahsanlma5e@gmail.com>
2026-09-06 21:52:14 +0300

    update en.json file

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:khatabook_lite/core/theme/app_colors.dart';
 import 'package:khatabook_lite/core/theme/app_text_styles.dart';
+import 'package:khatabook_lite/presentation/screens/backup_screen.dart';
 import 'package:khatabook_lite/presentation/screens/security_screen.dart';
 
 class SettingsScreen extends StatelessWidget {
